91c9340eae15dd4011769d5b8c1b1c03c4a5ab50
Unchanted <omkkdwivedi@gmail.com>
2024-11-10 10:48:05 +0530

    rename

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'favicon.svg'],
     manifest: {
       name: 'Annapurna',
       short_name: 'Annapurna',
